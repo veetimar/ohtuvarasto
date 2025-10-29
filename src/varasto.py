c19d925ca6fc8747fc63917cfbffdcb962f52ff5
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-10-29 17:22:30 +0200

    add bug

--- src/varasto.py
+++ src/varasto.py
@@ -11,7 +11,7 @@ class Varasto:
             self.saldo = 0.0
         elif alku_saldo <= tilavuus:
             # mahtuu
-            self.saldo = alku_saldo
+            self.saldo = 0
         else:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
